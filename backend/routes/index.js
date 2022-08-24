101a6bc0fba0bc4e1aa6c683e6b9ec83dfbbc200
alex dam <alexdam55@gmail.com>
2022-08-24 10:45:24 -0700

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,9 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
 
+router.use('/api', apiRouter);
 // backend/routes/index.js
 // ...
 // Add a XSRF-TOKEN cookie
