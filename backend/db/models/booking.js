cfc9816b03191cb0bd30f349078b7aa80e16f1db
alex dam <alexdam55@gmail.com>
2022-08-26 16:13:21 -0700

    added booking associations

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -11,6 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Booking.belongsTo(models.Spot, { foreignKey: 'spotId' })
+      Booking.belongsTo(models.User, { foreignKey: 'userId' })
     }
   }
   Booking.init({
