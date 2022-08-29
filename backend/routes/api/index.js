60d4be1723f00322bfc208b7aad23c8067313cb2
alex dam <alexdam55@gmail.com>
2022-08-29 13:03:34 -0700

    added /spots router

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,6 +2,7 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const spotsRouter = require('./spots')
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
@@ -9,6 +10,8 @@ router.use(restoreUser);
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+
+router.use('/spots', spotsRouter)
 
 router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
---
+++ backend/routes/api/spots.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { Spot } = require('../../db/models');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const spots = await Spot.findAll()
+    res.json(spots)
+})
+
+module.exports = router;
