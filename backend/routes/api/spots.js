8e094b6596c21094910ed0fd89ec817b7b40b6ee
alex dam <alexdam55@gmail.com>
2022-08-29 16:00:58 -0700

    create new spot

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -8,40 +8,41 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
+    const user = req.user
     const { address, city, state, country, lat, lng, name, description, price } = req.body
-    // try {
-    const newSpot = await Spot.create({
-        // ownerId: id,
-        address,
-        city,
-        state,
-        country,
-        lat,
-        lng,
-        name,
-        description,
-        price,
-    })
+    try {
+        const newSpot = await Spot.create({
+            ownerId: user.id,
+            address,
+            city,
+            state,
+            country,
+            lat,
+            lng,
+            name,
+            description,
+            price,
+        })
 
-    return res.json(newSpot)
-    // } catch {
-    //     res.statusCode = 400
-    //     res.json({
-    //         message: 'Validation Error',
-    //         statusCode: res.statusCode,
-    //         errors: {
-    //             "address": "Street address is required",
-    //             "city": "City is required",
-    //             "state": "State is required",
-    //             "country": "Country is required",
-    //             "lat": "Latitude is not valid",
-    //             "lng": "Longitude is not valid",
-    //             "name": "Name must be less than 50 characters",
-    //             "description": "Description is required",
-    //             "price": "Price per day is required"
-    //         }
-    //     })
-    // }
+        return res.json(newSpot)
+    } catch {
+        res.statusCode = 400
+        res.json({
+            message: 'Validation Error',
+            statusCode: res.statusCode,
+            errors: {
+                "address": "Street address is required",
+                "city": "City is required",
+                "state": "State is required",
+                "country": "Country is required",
+                "lat": "Latitude is not valid",
+                "lng": "Longitude is not valid",
+                "name": "Name must be less than 50 characters",
+                "description": "Description is required",
+                "price": "Price per day is required"
+            }
+        })
+    }
 
 })
 
