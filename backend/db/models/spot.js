2a5edc935ed78c8b9180ab5ac528ee1e579138e4
alex dam <alexdam55@gmail.com>
2022-08-29 15:46:08 -0700

    spot model changes

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -12,8 +12,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Spot.hasMany(models.SpotImage, { foreignKey: 'spotId' })
       Spot.belongsTo(models.User, { foreignKey: 'ownerId' })
+      Spot.hasMany(models.SpotImage, { foreignKey: 'spotId' })
       Spot.belongsToMany(models.User, {
         through: models.Booking,
         foreignKey: 'spotId',
