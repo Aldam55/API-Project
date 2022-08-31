3579271cca15d2c707c6d1d8288804b502570c1c
alex dam <alexdam55@gmail.com>
2022-08-31 15:35:05 -0700

    login failed credentials

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -35,10 +35,15 @@ router.post('/', async (req, res, next) => {
     const user = await User.login({ credential, password });
 
     if (!user) {
-        const err = new Error('Login failed');
-        err.status = 401;
-        err.title = 'Login failed';
-        err.errors = ['The provided credentials were invalid.'];
+        // const err = new Error('Login failed');
+        // err.status = 401;
+        // err.title = 'Login failed';
+        // err.errors = ['The provided credentials were invalid.'];
+        res.statusCode = 401
+        res.json({
+            message: "Invalid credentials",
+            statusCode: 401
+        })
         return next(err);
     }
 
