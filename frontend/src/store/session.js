79330d93bebf2866e80f12fb66ca8e54b25b5933
alex dam <alexdam55@gmail.com>
2022-09-14 15:43:44 -0700

    p1 done

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -27,7 +27,14 @@ export const login = (user) => async (dispatch) => {
         }),
     });
     const data = await response.json();
-    dispatch(setUser(data.user));
+    dispatch(setUser(data));
+    return response;
+};
+
+export const restoreUser = () => async dispatch => {
+    const response = await csrfFetch('/api/session');
+    const data = await response.json();
+    dispatch(setUser(data));
     return response;
 };
 
