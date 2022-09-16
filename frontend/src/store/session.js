bbed10f46a864d0af143c1acd1399e5a78a240ae
alex dam <alexdam55@gmail.com>
2022-09-16 13:05:11 -0700

    p2 first half

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -18,17 +18,19 @@ const removeUser = () => {
 };
 
 export const signup = (user) => async (dispatch) => {
-    const { username, email, password } = user;
+    const { firstName, lastName, username, email, password } = user;
     const response = await csrfFetch("/api/users", {
       method: "POST",
       body: JSON.stringify({
+        firstName,
+        lastName,
         username,
         email,
         password,
       }),
     });
     const data = await response.json();
-    dispatch(setUser(data.user));
+    dispatch(setUser(data));
     return response;
   };
 
