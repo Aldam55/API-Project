e9e94d1a37eb5548ed1097048cae8bd2ed0b47a6
alex dam <alexdam55@gmail.com>
2022-09-16 13:18:05 -0700

    p3 first half

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -55,6 +55,14 @@ export const restoreUser = () => async dispatch => {
     return response;
 };
 
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+      method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+  };
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
